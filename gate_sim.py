f0c48ad0c443778e73db82b7107a9f4eb53a1bc6
RCD2020 <dobertravis@icloud.com>
2024-07-17 21:42:05 -0600

    modified to support multiplexors

--- gate_sim.py
+++ gate_sim.py
@@ -6,5 +6,16 @@ def simulate_function(func):
             print(f'| {x} | {y} |  {int(func(x,y))}  |')
 
 
+def simulate_absel(func):
+    print('| a | b | sel | out |')
+    print('|---|---|-----|-----|')
+    for sel in range(0,2):
+        for a in range(0,2):
+            for b in range(0,2):
+                print(
+                    f'| {a} | {b} |  {sel}  |  {int(func(a,b,sel))}  |'
+                )
+
+
 if __name__ == '__main__':
     simulate_function(max)
