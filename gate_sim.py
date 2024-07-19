a7329a12b863936eea66bc9eba0f71f9c49dc906
RCD2020 <dobertravis@icloud.com>
2024-07-18 18:30:34 -0600

    added function to simulate multibits

--- gate_sim.py
+++ gate_sim.py
@@ -1,3 +1,6 @@
+from multibit_repr import generate_multi, multi2str
+
+
 def simulate_function(func):
     print('| a | b | out |')
     print('|---|---|-----|')
@@ -24,6 +27,17 @@ def simulate_insel(func):
         for gate_in in range(0,2):
             a, b = func(gate_in, sel)
             print(f'| {gate_in}  |  {sel}  || {int(a)} | {int(b)} |')
+
+
+def simulate_multibit(func):
+    multibits = generate_multi(2)
+
+    print('| a  | b  | out |')
+    print('|----|----|-----|')
+    for a in multibits:
+        for b in generate_multi(2):
+            print(f'| {multi2str(a)} | {multi2str(b)} '
+                + f'| {multi2str(func(a ,b))}  |')
 
 
 if __name__ == '__main__':
