c11edca770dae6b18c663e570da6334589ee89d9
RCD2020 <dobertravis@icloud.com>
2024-08-02 18:28:56 -0600

    implemented 16bit multiplexor in hdl

--- multi_gen.py
+++ multi_gen.py
@@ -1,4 +1,4 @@
 for i in range(4):
     for i2 in range(4):
-        print(f'Or(a=a[{4*i+i2}], b=b[{4*i+i2}], out=out[{4*i+i2}]);')
+        print(f'Mux(a=a[{4*i+i2}], b=b[{4*i+i2}], sel=sel, out=out[{4*i+i2}]);')
     print()
